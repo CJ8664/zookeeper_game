5a7aaa7e49d16d70cea4b23b18232f5898b05c90
Chirag Jain <csjain@ncsu.edu>
2018-09-07 18:38:55 -0400

    Added random score generation

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ import sys
 
 from kazoo.client import KazooClient
 from kazoo.recipe.queue import Queue
+from numpy import random
 
 logging.basicConfig()
 zk = KazooClient(hosts='127.0.0.1:2181')
@@ -14,7 +15,10 @@ print('Client begin')
 data = 0
 my_queue = Queue(zk, "/queue")
 
+mu, sigma = 0, 0.1 # mean and standard deviation
+
 while True:
+    data = int(round(abs(random.normal(mu, sigma, 1) * 1000000)))
     my_queue.put((sys.argv[1] + ':' + str(data)).encode('utf-8'))
     time.sleep(1)
     print('Value set: {}'.format(data))
