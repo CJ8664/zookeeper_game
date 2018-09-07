e9a24e32652f83b13c781497a6f9f0e8f5b688e6
Chirag Jain <csjain@ncsu.edu>
2018-09-07 18:21:17 -0400

    basic version working

--- client.py
+++ client.py
@@ -2,19 +2,20 @@
 
 import logging
 import time
+import sys
 
 from kazoo.client import KazooClient
 from kazoo.recipe.queue import Queue
 
+logging.basicConfig()
 zk = KazooClient(hosts='127.0.0.1:2181')
 zk.start()
 print('Client begin')
 data = 0
 my_queue = Queue(zk, "/queue")
+
 while True:
-    # zk.set("/queue", str(data).encode())
-    # zzz = bytes(, 'utf')
-    my_queue.put(str(data).encode('utf-8'))
+    my_queue.put((sys.argv[1] + ':' + str(data)).encode('utf-8'))
     time.sleep(1)
     print('Value set: {}'.format(data))
     data += 1
---
+++ watcher.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python3
+
+import logging
+import time
+
+from kazoo.recipe.watchers import ChildrenWatch
+from kazoo.client import KazooClient
+from kazoo.recipe.queue import Queue
+
+
+class ScoreWatcher:
+
+    curr_score = []
+
+    def __init__(self):
+        logging.basicConfig()
+        self.zk = KazooClient(hosts='127.0.0.1:2181')
+        self.zk.start()
+        print('Watch begin')
+        self.my_queue = Queue(self.zk, "/queue")
+        cw = ChildrenWatch(self.zk, "/queue", self.process_score)
+
+
+    def print_recent_board(self):
+        print('Most recent scores')
+        print('------------------')
+        for player, score in self.curr_score:
+            print('{}\t\t{}'.format(player, score))
+        print('\n')
+
+
+    def print_leader_board(self):
+        print('Highest scores')
+        print('--------------')
+        for player, score in sorted(self.curr_score, key=lambda x: int(x[1]), reverse=True):
+            print('{}\t\t{}'.format(player, score))
+        print('\n')
+
+
+    def process_score(self, children):
+        chil = self.my_queue.get()
+        if chil:
+            if not self.curr_score:
+                self.curr_score = [chil.split(':')]
+            elif len(self.curr_score) < 20:
+                self.curr_score = [chil.split(':')] + self.curr_score
+            else:
+                self.curr_score = [chil.split(':')] + self.curr_score[:-1]
+            self.print_recent_board()
+            self.print_leader_board()
+
+
+def main():
+    sw = ScoreWatcher()
+    while True:
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
