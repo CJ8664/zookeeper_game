eb3c0fa79cea981270df74c704576919a9a04ec4
Chirag Jain <csjain@ncsu.edu>
2018-09-20 14:52:46 -0400

    Updated test script

--- test_client.sh
+++ test_client.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 echo "Starting Player Captain America"
-python player.py "Captain America" &
+player $1 "Captain America" 100 5000 2 &
 echo "Starting Player Thor"
-python player.py "Thor" &
+player $1 "Thor" 100 5000 3 &
 echo "Starting Player Smaug"
-python player.py "Smaug" &
+player $1 "Smaug" 100 5000 4 &
 echo "Starting Player Bob"
-python player.py "Bob" &
+player $1 "Bob" 100 5000 5 &
 echo "Starting Player Prof Freeh"
-python player.py "Prof Freeh" &
+player "Prof Freeh" 100 5000 6 &
+echo "Starting Player Chirag"
+player $1 Chirag
 
 # for i in `ps awx | grep -i python | awk -F" " '{print $1}'`
 # do
